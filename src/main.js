e48333a4040fca23c61237441374cc50569935e8
Andy Escobar <andyescobar007@unitec.edu>
2018-06-19 11:07:47 -0600

    Finish 1.01

--- src/main.js
+++ src/main.js
@@ -1,88 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
 
 
-let config = {
-  apiKey: "AIzaSyCDtLH2nqHR49q3a0eh8Y5BlJIOqIag4Es",
-  authDomain: "sistemadecitas-6308c.firebaseapp.com",
-  databaseURL: "https://sistemadecitas-6308c.firebaseio.com",
-  projectId: "sistemadecitas-6308c",
-  storageBucket: "sistemadecitas-6308c.appspot.com",
-  messagingSenderId: "713895614996"
-};
 
-let app = firebase.initializeApp(config);
-let db = app.database();
+// firebase
+import VueFire from 'vuefire';
 
-let citasmedicasRef = db.ref('citasmedicas');
 
-var vm = new Vue({
-  el: "#app",
-  ready() {
-    citasmedicasRef.on('value',function (snapshot){ 
-        console.log('hola');
-    });
-  },
 
-  data: {
-    // Parametros
+Vue.config.productionTip = false
 
-    newEntry: {
-      n_identidad: "",
-      paciente: "",
-      fecha: ""
-    },
-    items: [ ],
-  },
+Vue.use(VueFire);
+Vue.use(VueMask.VueMaskPlugin);
 
-  methods: {
-    remove: function(index) {
-      this.items.splice(index, 1);
-    },
-    // Ecmascript 6 syntax
-    add: function() {
-      if (this.newEntry.n_identidad.length > 14) {
-        if (this.newEntry.paciente.length > 0) {
-          if (this.newEntry.fecha.length > 9) {
-            citasmedicasRef.push({
-              n_identidad: this.newEntry.n_identidad,
-              paciente: this.newEntry.paciente,
-              fecha: this.newEntry.fecha,
-              paid: false
-            });
-            // this.items.push({
-            //   n_identidad: this.newEntry.n_identidad,
-            //   paciente: this.newEntry.paciente,
-            //   fecha: this.newEntry.fecha,
-            //   paid: false
-            // });
-            this.newEntry.n_identidad = "";
-            this.newEntry.paciente = "";
-            this.newEntry.fecha = "";
-            this.$toastr.success("Cita Agregada");
-          } else {
-            this.$toastr.error("Ingrese una fecha valida");
-          }
-        } else {
-          this.$toastr.error("Porfavor ingrese un nombre al paciente");
-        }
-      } else {
-        this.$toastr.error("Numero de Identidad no valido");
-      }
-    },
-    changeToPaid: function(item) {
-      item.paid = !item.paid;
-    },
-    totalAmount(t) {
-      var total = this.items.reduce(function(a, b) {
-        switch (t) {
-          case 0:
-            return a + (!b.paid ? b.amount : 0);
-          case 1:
-            return a + (b.paid ? b.amount : 0);
-          case 2:
-            return a + b.amount;
-        }
-      }, 0);
-      return total.toFixed(2);
-    }
-  }
-});
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
